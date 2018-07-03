82ca7dd15f943dcde5c7509422e3f061af258b98
Moira Tagle <moirat@wepay.com>
2018-07-03 13:40:54 -0700

    I'm not 100% sure what's in here but I want to move onto some other stuff and I'll clean it up later.

--- debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/AbstractReader.java
+++ debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/AbstractReader.java
@@ -96,6 +96,7 @@ public abstract class AbstractReader implements Reader {
 
     @Override
     public void start() {
+        logger.info("ATTEMPTING TO START READER {}", name);
         if (this.running.compareAndSet(false, true)) {
             this.failure.set(null);
             this.success.set(false);
@@ -105,6 +106,7 @@ public abstract class AbstractReader implements Reader {
 
     @Override
     public void stop() {
+        logger.info("ATTEMPTING TO STOP READER {}", name);
         try {
             doStop();
             running.set(false);
